d9ecf9a2a7b5bda59f7dc8e1d79acba18acecc85
Omar Baba <omar+1@posit.co>
2024-05-07 10:52:11 -0400

    Create app_with_logs.py

---
+++ app_with_logs.py
@@ -0,0 +1,33 @@
+import platform
+import subprocess
+
+from shiny import App, render, ui
+
+app_ui = ui.page_fluid(
+    ui.h2("Tool versions:"),
+    ui.span("Python"),
+    ui.output_text_verbatim("python"),
+    ui.input_text_area("cmd", "Command to run", placeholder="Enter text"),
+    ui.output_text_verbatim("cmd_output"),
+)
+
+
+def server(input, output, session):
+    @output
+    @render.text
+    def python():
+        return platform.python_version()
+
+    @output
+    @render.text
+    def cmd_output():
+        cmd=input.cmd()
+        try:
+              res = subprocess.check_output(cmd, shell=True).decode()
+              print(f"result: {res}")
+              return res
+        except Exception as e:
+            return f"Error: {e}"
+
+
+app = App(app_ui, server)
